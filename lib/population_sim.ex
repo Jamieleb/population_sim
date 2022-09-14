971a2c1b0950b134411255792beb420f9241540b
James C Le Brun <jamesclebrun@Jamess-MacBook-Air.local>
2022-09-14 19:27:49 +0100

    feat: add fns to handle edge cases and aid live neighbours calc

--- lib/population_sim.ex
+++ lib/population_sim.ex
@@ -3,6 +3,16 @@ defmodule PopulationSim do
   Documentation for `PopulationSim`.
   """
 
+  def neighbouring_indices({x_position, y_position}) do
+    x_range = range_within_boundary(x_position)
+    y_range = range_within_boundary(y_position)
+    {x_range, y_range}
+  end
+
+  def range_within_boundary(0), do: 0..1
+  def range_within_boundary(8), do: 7..8
+  def range_within_boundary(position), do: (position - 1)..(position + 1)
+
   def neighbouring_cells({x_range, y_range}, grid) do
     Enum.to_list(y_range)
     |> Enum.map(&Enum.at(grid, &1))
@@ -11,5 +21,9 @@ defmodule PopulationSim do
 
   def value_at_coordinates({x_pos, y_pos}, grid) do
     grid |> Enum.at(y_pos) |> Enum.at(x_pos)
+  end
+
+  def sub_value_from_cell(total, cell, grid) do
+    total - value_at_coordinates(cell, grid)
   end
 end
--- test/population_sim_test.exs
+++ test/population_sim_test.exs
@@ -38,6 +38,34 @@ defmodule PopulationSimTest do
     [0, 0, 0, 0, 0, 0, 0, 0, 0]
   ]
 
+  describe "range_within_boundary function" do
+    test "for central cell" do
+      assert PopulationSim.range_within_boundary(4) == 3..5
+    end
+
+    test "for cell at first row or column" do
+      assert PopulationSim.range_within_boundary(0) == 0..1
+    end
+
+    test "for cell at final row or column" do
+      assert PopulationSim.range_within_boundary(8) == 7..8
+    end
+  end
+
+  describe "value at coordinates function" do
+    test "central cell" do
+      assert PopulationSim.value_at_coordinates({3, 2}, @initial_grid) == 1
+    end
+
+    test "first row cell" do
+      assert PopulationSim.value_at_coordinates({3, 0}, @initial_grid) == 0
+    end
+
+    test "last column cell" do
+      assert PopulationSim.value_at_coordinates({8, 4}, @initial_grid) == 0
+    end
+  end
+
   describe "neighbouring_cells" do
     test "neighbouring cells function for central cell" do
       assert PopulationSim.neighbouring_cells({3..5, 2..4}, @initial_grid) == [
